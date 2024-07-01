072659e4478bafd8f0d3018862c4f69197835f3d
John Perez <mrjohnjairo10@gmail.com>
2024-07-01 17:39:33 -0500

    feat(code): add datastructure and recursion
    - Add `array` datastructure
    - Add Tower of Hanoi problem using recursion

---
+++ src/data_structures/array.rs
@@ -0,0 +1,6 @@
+pub fn array() {
+    println!("Array with fixed length:");
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+
+    println!("{:?}", arr)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod data_structures;
+
 fn main() {
+    data_structures::array::array();
+    print_line_separator();
+    towers_of_hanoi(3, 'A', 'C', 'B');
+
     println!("Hello, world!");
+}
+
+fn print_line_separator() {
+    println!("--------------------------------------------");
+}
+
+fn towers_of_hanoi(n: i32, frompeg: char, topeg: char, auxpeg: char) {
+    // Base case
+    if n == 1 {
+        println!("Move disk 1 from peg {} to peg {}", frompeg, topeg);
+        print_line_separator();
+        return;
+    }
+
+    // Ecursion, Move n-1 disks from `frompeg` to `auxpeg`
+    towers_of_hanoi(n - 1, frompeg, auxpeg, topeg);
+
+    // Move remaining disks from A to C
+    println!("Move disk {} from peg {} to peg {}", n, frompeg, topeg);
+
+    // Move n-1 disks from B to C using A as auxiliary
+    towers_of_hanoi(n - 1, auxpeg, topeg, frompeg);
 }
