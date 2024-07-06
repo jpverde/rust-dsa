0178262ed9cae2ed1ed71d39bf363ff814bb7ed4
John Perez <mrjohnjairo10@gmail.com>
2024-07-06 15:19:50 -0500

    refactor(code): uncomment examples
    - Uncomment examples of code
    - Add new methods to Points

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use core::fmt;
 mod data_structures;
 
 fn main() {
-    //data_structures::array::array();
-    //print_line_separator();
-    //towers_of_hanoi(3, 'A', 'C', 'B');
+    data_structures::array::array();
+    print_line_separator();
+    towers_of_hanoi(3, 'A', 'C', 'B');
     data_structures::single_linked_list::linked_list();
     print_line_separator();
-    //let point = Point::new(3.46, 5.67);
-    //let point_str = Point::new("some", "text");
-    //println!("Point: {} {}", point_str, point.get_x());
-    //println!("Point: {} {}", new_val, new_val.get_x());
-    println!("Hello, world!");
+    let mut point = Point::new(3.46, 5.67);
+    let point_str = Point::new("some", "text");
+    println!("Point: {} {}", point_str, point.get_x());
+    point.update(3.0, 3.0);
+    println!("Point: {} {}", point.get_x(), point.get_y());
 }
 
 fn print_line_separator() {
@@ -51,8 +51,13 @@ impl<T> Point<T> {
         &self.x
     }
 
-    fn update(&mut self, x: T, y: T) -> Self {
-        Self { x, y }
+    fn get_y(&self) -> &T {
+        &self.y
+    }
+
+    fn update(&mut self, x: T, y: T) {
+        self.x = x;
+        self.y = y;
     }
 }
 
